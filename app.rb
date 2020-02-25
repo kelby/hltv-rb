2642977d39011fa2f139e8c5b1144b7204f7e9c0
Kelby Lee <leekelby@gmail.com>
2020-02-25 13:21:48 +0800

    更新抓取代码；注意cookie里的cf_clearance过期问题

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 require "bundler/setup"
 
 require 'sinatra'
-require "./lib/hltv/request"
+require "./lib/hltv/fetcher"
 
 # Bundler.require
 
@@ -11,29 +11,33 @@ get '/ping' do
 end
 
 get '/get_matches' do
-  Hltv::Request.new.get_matches.to_json
+  Hltv::Fetcher.new.get_matches.to_json
 end
 
 get '/get_match/:id' do
-  Hltv::Request.new.get_match(params[:id]).to_json
+  Hltv::Fetcher.new.get_match(params[:id]).to_json
+end
+
+get '/get_event/:id' do
+  Hltv::Fetcher.new.get_event(params[:id]).to_json
 end
 
 get '/matches_mapstatsid/:mapstatsid' do
-  Hltv::Request.new.matches_mapstatsid(params[:mapstatsid]).to_json
+  Hltv::Fetcher.new.matches_mapstatsid(params[:mapstatsid]).to_json
 end
 
 get '/stats_maps' do
-  Hltv::Request.new.stats_maps(params[:event_id], params[:start_date], params[:end_date]).to_json
+  Hltv::Fetcher.new.stats_maps(params[:event_id], params[:start_date], params[:end_date]).to_json
 end
 
 get '/stats_players' do
-  Hltv::Request.new.stats_players(params[:player_id], params[:event_id], params[:start_date], params[:end_date]).to_json
+  Hltv::Fetcher.new.stats_players(params[:player_id], params[:event_id], params[:start_date], params[:end_date]).to_json
 end
 
 get '/stats_teams' do
-  Hltv::Request.new.stats_teams(params[:team_id], params[:start_date], params[:end_date]).to_json
+  Hltv::Fetcher.new.stats_teams(params[:team_id], params[:start_date], params[:end_date]).to_json
 end
 
 get '/stats_teams_maps' do
-  Hltv::Request.new.stats_teams_maps(params[:team_id], params[:start_date], params[:end_date]).to_json
+  Hltv::Fetcher.new.stats_teams_maps(params[:team_id], params[:start_date], params[:end_date]).to_json
 end
--- lib/hltv/fetcher.rb
+++ lib/hltv/fetcher.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'pry'
 
 module Hltv
-  class Request
+  class Fetcher
     def get_matches
       request = Typhoeus.get("https://www.hltv.org/matches")
 
@@ -590,17 +590,18 @@ module Hltv
 
     def headers
       _headers = {}
+
       _headers["Authority"] = "www.hltv.org"
-      _headers["Cache-Control"] = "max-age=0"
       _headers["Upgrade-Insecure-Requests"] = "1"
-      _headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
+      _headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36"
       _headers["Sec-Fetch-Dest"] = "document"
       _headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
       _headers["Sec-Fetch-Site"] = "none"
       _headers["Sec-Fetch-Mode"] = "navigate"
       _headers["Sec-Fetch-User"] = "?1"
       _headers["Accept-Language"] = "zh-CN,zh;q=0.9,en;q=0.8,pt;q=0.7,zh-TW;q=0.6,fr;q=0.5,ru;q=0.4,ja;q=0.3,es;q=0.2"
-      _headers["Cookie"] = "_ga=GA1.2.808081295.1539914234; MatchFilter={%22active%22:false%2C%22live%22:false%2C%22stars%22:1%2C%22lan%22:false%2C%22teams%22:[]}; __cfduid=d0e30472136868a1443bc538fdb43f5701571500671; _gid=GA1.2.1544633397.1581413155; statsTablePadding=small; cf_clearance=c6a98493976373aa1fd1a06b5e6c85c62bbc9f06-1581902558-0-150"
+      _headers["Cookie"] = "_ga=GA1.2.808081295.1539914234; MatchFilter={%22active%22:false%2C%22live%22:false%2C%22stars%22:1%2C%22lan%22:false%2C%22teams%22:[]}; __cfduid=d0e30472136868a1443bc538fdb43f5701571500671; statsTablePadding=small; _gid=GA1.2.1867843134.1582457537; cf_clearance=b28297f5494e597750b22f278b2890a044f1061f-1582586100-0-150"
+
       _headers
     end
   end
